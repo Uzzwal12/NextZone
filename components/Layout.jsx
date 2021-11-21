81a8cf5e65fea9b9193bab29ac7673b05d829532
Uzzwal Gupta <uzzwal.gupta@squareboat.com>
2021-11-21 22:02:04 +0530

    worked on dark theme and light theme

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,38 +1,84 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import NextLink from "next/link";
-import { AppBar, Container, Link, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Switch,
+} from "@mui/material";
 import useStyles from "../utils/styles";
+import { Store } from "../utils/store";
+
 export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
   const classes = useStyles();
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: "normal",
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: "normal",
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
+
+  const darkModeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+  };
+
   return (
     <div>
       <Head>
         <title>{title ? title : "NextZone"}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/" passHref>
-            <Link>
-              <Typography className={classes.brand}>NextZone</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>NextZone</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All Rights Reserved. NextZone.</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch checked={darkMode} onChange={darkModeHandler}></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All Rights Reserved. NextZone.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
